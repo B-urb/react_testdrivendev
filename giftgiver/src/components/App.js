d3b3bb288a87305526b634910c7e7f195fb542e0
B u <bjoern.urban@protonmail.com>
2018-07-08 10:30:23 +0200

    Final commit in giftgiver

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,5 +1,7 @@
 import React,{Component} from "react";
 import {Button} from "react-bootstrap";
+import Gift from "./Gift";
+import {max_number} from "../helper";
 
 
 class App extends Component
@@ -13,18 +15,32 @@ class App extends Component
 
     addGift = () => {
       const {gifts} = this.state;
-      const ids = this.state.gifts.map(gift => gift.id);
-
-      const max_id = ids.length > 0 ? Math.max(...ids) : 0;
-      gifts.push({id: max_id+1 })
+        gifts.push({id: max_number(this.state.gifts.map(gift => gift.id)) + 1 });
         this.setState({gifts})
 
+    };
+
+    removeGift = id => {
+        const gifts = this.state.gifts.filter(gift => gift.id !== id)
+        this.setState({gifts})
     };
 
     render() {
         return (
             <div>
                 <h2>Gift geber</h2>
+                <div className="gift-list">
+                    {
+                        this.state.gifts.map(gift => {
+                            return (
+                                <Gift
+                                key={gift.id}
+                                gift={gift}
+                                removeGift={this.removeGift}
+                                />)
+                        })
+                    }
+                </div>
                 <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
             </div>
         );
